2da1967a4138d83dafb20675e13111bdd739b678
ngautam222 <76139038+ngautam222@users.noreply.github.com>
2021-08-20 16:03:47 -0400

    o

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-import tkinter 
+from tkinter.filedialog import askdirectory
+import tkinter
 import pygame
 import os
+from tkinter.constants import SINGLE
 
 player = tkinter.Tk()
 player.title("Nicks player")
 player.geometry("400x400")
+
+dir = askdirectory()
+os.chdir(dir)
+songs = os.listdir()
+
+lst = tkinter.Listbox(player,font="Helvetica",bg="white",selectmode=SINGLE)
+
+
+player.mainloop()
